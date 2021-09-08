d096b808f742ae25d12a56ffbfbb302c1052fca1
alexeybachaev <bachaev2000@yandex.ru>
2021-09-08 16:59:34 +0300

    Доделал сайдбар

--- scripts/sidebar-menu.js
+++ scripts/sidebar-menu.js
@@ -1,1 +1,11 @@
-console.log("Hello gigachad")
+const openingBtn = document.querySelector('.sidebar__hamburger')
+const closingBtn = document.querySelector('.sidebar__close')
+const sideBar = document.querySelector('.sidebar')
+
+openingBtn.addEventListener("click", function () {
+    sideBar.classList.add("sidebar--opened")
+})
+
+closingBtn.addEventListener("click", function () {
+    sideBar.classList.remove("sidebar--opened")
+})
