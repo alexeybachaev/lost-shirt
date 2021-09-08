993aa4aee5419f28f12b7b4fe70920075c583959
alexeybachaev <bachaev2000@yandex.ru>
2021-09-08 17:28:28 +0300

    Сделал слайдер

--- scripts/sidebar-menu.js
+++ scripts/sidebar-menu.js
@@ -1,11 +1,15 @@
-const openingBtn = document.querySelector('.sidebar__hamburger')
-const closingBtn = document.querySelector('.sidebar__close')
-const sideBar = document.querySelector('.sidebar')
+//Сделал IIFE чтобы нельзя было 
+//использовать переменные в других скриптах
+(function () {
+    const openingBtn = document.querySelector('.sidebar__hamburger')
+    const closingBtn = document.querySelector('.sidebar__close')
+    const sideBar = document.querySelector('.sidebar')
 
-openingBtn.addEventListener("click", function () {
-    sideBar.classList.add("sidebar--opened")
-})
+    openingBtn.addEventListener("click", function () {
+        sideBar.classList.add("sidebar--opened")
+    })
 
-closingBtn.addEventListener("click", function () {
-    sideBar.classList.remove("sidebar--opened")
-})
+    closingBtn.addEventListener("click", function () {
+        sideBar.classList.remove("sidebar--opened")
+    })
+}())
